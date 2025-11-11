bc7a5fe8223d139c132580a4b11f8238433911ce
Hunter <huntercmcgraw@gmail.com>
2025-11-11 16:54:04 -0500

    separated client and resource

--- src/pnw-csharp/Client/PnWAPIClient.cs
+++ src/pnw-csharp/Client/PnWAPIClient.cs
@@ -1,5 +1,6 @@
 using RestSharp;
 using PnW.Models;
+using System.Reflection;
 
 namespace PnW.Client
 {
@@ -22,18 +23,24 @@ namespace PnW.Client
             _client = new RestClient(GraphQLUrl);
         }
 
-        public async Task<T> GetData<T>(string queryType, string id, List<string> fieldList)
+        private string LoadResourceQuery(string resourceName)
         {
-            string fields = string.Join("\n", fieldList);
-            string query = $@"
-            query {{
-            {queryType}(id: [{id}]) {{
-                data {{
-                    {fields}
-                }}
-            }}
-            }}";
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceFullName = $"PnW.Resources.{resourceName}.graphql";
 
+            using Stream? stream = assembly.GetManifestResourceStream(resourceFullName);
+
+            if (stream == null)
+            {
+                throw new InvalidOperationException($"Could not find embedded resource: {resourceFullName}.");
+            }
+
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
+        public async Task<T> ExecuteGraphQLQueryAsync<T>(string query, string queryType, string id)
+        {
             var request = new RestRequest("/", Method.Post);
             request.AddJsonBody(new { query = query });
 
@@ -42,8 +49,8 @@ namespace PnW.Client
 
             if (response.IsSuccessful && response.Data?.Data?.Count > 0)
             {
-                var root = response.Data?.Data;
-                if (root != null && root.TryGetValue(queryType, out var paginator) && paginator?.Data?.Count > 0)
+                var root = response.Data.Data;
+                if (root.TryGetValue(queryType, out var paginator) && paginator?.Data?.Count > 0)
                 {
                     return paginator.Data[0];
                 }
@@ -55,8 +62,16 @@ namespace PnW.Client
 
         public async Task<T> GetQuery<T>(string targetId, List<string> fieldList)
         {
-            string queryType = _map[typeof(T)];
-            return await GetData<T>(queryType, targetId, fieldList);
+            if (!_map.TryGetValue(typeof(T), out string? queryType))
+            {
+                 throw new InvalidOperationException($"Type {typeof(T).Name} is not mapped to a GraphQL resource.");
+            }
+            string queryTemplate = LoadResourceQuery("GetResource");
+            string fields = string.Join("\n", fieldList);
+            
+            string finalQuery = string.Format(queryTemplate, queryType, targetId, fields);
+
+            return await ExecuteGraphQLQueryAsync<T>(finalQuery, queryType, targetId);
         }
     }
 
